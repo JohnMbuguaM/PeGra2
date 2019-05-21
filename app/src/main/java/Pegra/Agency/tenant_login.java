3dcce3cc6e716283fe9d112e70e76a81a64d9f0f
mbugua home <mbuguamuthoni9@gmail.com>
2019-05-21 19:29:55 +0300

    ui commit

---
+++ app/src/main/java/Pegra/Agency/tenant_login.java
@@ -0,0 +1,13 @@
+package Pegra.Agency;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class tenant_login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenant_login);
+    }
+}
